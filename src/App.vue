647cf6cb33da35c113413731277196d1479f44b2
S3CR3T <33022922+kim0tae@users.noreply.github.com>
2023-03-07 21:11:06 +0900

    컴포넌트 생성 및 등록

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,25 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <TodoHeader></TodoHeader>
+    <TodoInput></TodoInput>
+    <TodoList></TodoList>
+    <TodoFooter></TodoFooter>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import TodoFooter from './components/TodoFooter.vue';
+import TodoHeader from './components/TodoHeader.vue';
+import TodoInput from './components/TodoInput.vue';
+import TodoList from './components/TodoList.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    TodoFooter,
+    TodoHeader,
+    TodoInput,
+    TodoList,
   }
 }
 </script>
